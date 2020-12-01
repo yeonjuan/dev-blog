fd4e6767f8e34803583ce5e38a3e05c9d470079f
yeonjuan <yeonjuan93@naver.com>
2020-12-02 00:52:02 +0900

    init page

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  overrides: [
+    {
+      files: ["*.html"],
+      plugins: ["@html-eslint"],
+      parser: "@html-eslint/parser",
+      extends: ["plugin:@html-eslint/recommended"],
+      rules: {
+        "@html-eslint/require-doctype": "error",
+        "@html-eslint/no-duplicate-id": "error",
+        "@html-eslint/no-inline-styles": "error",
+        "@html-eslint/require-li-container": "error",
+        "@html-eslint/no-obsolete-tags": "error",
+        "@html-eslint/require-lang": "error",
+        "@html-eslint/require-title": "error",
+        "@html-eslint/no-multiple-h1": "error",
+        "@html-eslint/require-img-alt": "error",
+        "@html-eslint/no-skip-heading-levels": "error",
+        "@html-eslint/indent": "error",
+        "@html-eslint/element-newline": "error",
+        "@html-eslint/no-extra-spacing-attrs": "error",
+        "@html-eslint/quotes": "error",
+        "@html-eslint/id-naming-convention": "error",
+        "@html-eslint/require-closing-tags": "error"
+      }
+    },
+  ],
+};
