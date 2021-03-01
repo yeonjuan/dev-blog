549bb63f27866086c1c7424430d59c6b22eb81b9
yeonjuan <yeonjuan93@naver.com>
2021-03-01 21:00:22 +0900

    Updates

---
+++ 21.render-page.js
@@ -0,0 +1,33 @@
+exports.ids = [21];
+exports.modules = {
+
+/***/ "./node_modules/@deckdeckgo/highlight-code/dist/esm/shades-of-purple-649a9b24.js":
+/*!***************************************************************************************!*\
+  !*** ./node_modules/@deckdeckgo/highlight-code/dist/esm/shades-of-purple-649a9b24.js ***!
+  \***************************************************************************************/
+/*! exports provided: theme */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "theme", function() { return theme; });
+const theme = `{
+  --deckgo-highlight-code-carbon-background: #2d2b55;
+  --deckgo-highlight-code-carbon-color: #fff;
+  --deckgo-highlight-code-token-atrule: #ff9d00;
+  --deckgo-highlight-code-token-comment: #b362ff;
+  --deckgo-highlight-code-token-function: #9effff;
+  --deckgo-highlight-code-token-operator: #ff9d00;
+  --deckgo-highlight-code-token-property: #fad000;
+  --deckgo-highlight-code-token-regex: rgb(165, 255, 144);
+  --deckgo-highlight-code-token-selector: #ffffff;
+  --deckgo-highlight-code-token-punctuation: #fff;
+}`;
+
+
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=21.render-page.js.map
---
+++ 6.render-page.js
@@ -0,0 +1,33 @@
+exports.ids = [6];
+exports.modules = {
+
+/***/ "./node_modules/@deckdeckgo/highlight-code/dist/esm/cobalt-5d681c9a.js":
+/*!*****************************************************************************!*\
+  !*** ./node_modules/@deckdeckgo/highlight-code/dist/esm/cobalt-5d681c9a.js ***!
+  \*****************************************************************************/
+/*! exports provided: theme */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "theme", function() { return theme; });
+const theme = `{
+  --deckgo-highlight-code-carbon-background: #002240;
+  --deckgo-highlight-code-carbon-color: #fff;
+  --deckgo-highlight-code-token-atrule: #ffee80;
+  --deckgo-highlight-code-token-comment: #08f;
+  --deckgo-highlight-code-token-function: #fff;
+  --deckgo-highlight-code-token-property: #ff80e1;
+  --deckgo-highlight-code-token-punctuation: #d8d8d8;
+  --deckgo-highlight-code-token-regex: #3ad900;
+  --deckgo-highlight-code-token-selector: #9effff;
+  --deckgo-highlight-code-token-operator: #ffffff;
+}`;
+
+
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=6.render-page.js.map
