99215a8a5fffa7bd9ca25bb165e6c7f0b5a14bff
YeonJuan <yeonjuan93@naver.com>
2025-01-25 00:40:39 +0900

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import { html } from '@html-kit/html'
-import devPostList from "../components/dev-post-list.js";
-import layout from "../components/layout.js";
+import devPostList from '../components/dev-post-list.js'
+import layout from '../components/layout.js'
 
 export default (_, data) => layout({
-  title: "DevBlog",
+  title: 'DevBlog',
   children: html`
-    <h2 class="py-8 text-2xl">개발</h2>
-    ${devPostList(data("posts").posts)}
-`
-});
+    ${devPostList(data('posts').posts)}
+`,
+})
--- src/pages/post.js
+++ src/pages/post.js
@@ -1,11 +1,11 @@
 import { html } from '@html-kit/html'
-import layout from '../components/layout.js';
+import layout from '../components/layout.js'
 
-export default (children) => layout({
-  title: "DevBlog",
+export default children => layout({
+  title: 'DevBlog',
   children,
   link: html`
     <link href="/css/github.min.css" type="text/css" rel="stylesheet" />
     
-    `
+    `,
 })
