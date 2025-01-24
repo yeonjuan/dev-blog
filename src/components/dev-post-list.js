708517a8eb50b2f3a41db217c861a69446d7c249
YeonJuan <yeonjuan93@naver.com>
2025-01-24 23:28:51 +0900

    update

--- src/components/dev-post-list.js
+++ src/components/dev-post-list.js
@@ -1,19 +1,23 @@
-import { html } from "@html-kit/html"
+import { html } from '@html-kit/html'
 
 const postItem = post => html`
   <div class="border-t border-black pt-6 mb-16">
-    <span class="text-xs font-bold block">${post.category}</span>
+    <div class="w-full flex">
+      <span class="text-xs font-bold block">${post.category}</span>
+      <span class="text-xs ml-auto mr-0">${post.createdAt}</span>
+    </div>
     <a href="/posts/JavaScript/speeding-up-the-javascript-ecosystem-the-barrel-file-debacle/">
       <span class="block my-3 text-xl font-bold">${post.title}</span>
+   
       <p class="text-sm text-zinc-600 mb-8">
-                ${post.description}
+          ${post.description}
       </p>
       <img src="${post.thumbnail}" alt="">
     </a>
   </div>
 `
 
-export default (posts) => html`
+export default posts => html`
   <div>
         ${
           posts.map(post => postItem(post)).join('\n')
